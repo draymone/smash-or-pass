747014729a96a82d523344c039784d95a2011e7d
draymone <socotcho@gmail.com>
2023-12-29 23:04:20 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace SmashOrPass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int smash;
+
+        private int pass;
+
+        private int actualImage;
+        private int numberOfImages;
+
+        private const string pathPrefix = "D:\\Documents\\SmashOrPass\\";
+        private const string pathSuffix = ".jpg";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            numberOfImages = Directory.GetFiles(pathPrefix).Length;
+        }
+
+        private void SmashClick(object sender, RoutedEventArgs e)
+        {
+            smash++;
+            DisplaySmash();
+            NextImage();
+        }
+
+        private void PassClick(object sender, RoutedEventArgs e)
+        {
+            pass++;
+            DisplayPass();
+            NextImage();
+        }
+
+        private void CancelSmash(object sender, RoutedEventArgs e)
+        {
+            smash--;
+            DisplaySmash();
+            PreviousImage();
+        }
+
+        private void CancelPass(object sender, RoutedEventArgs e)
+        {
+            pass--;
+            DisplayPass();
+            PreviousImage();
+        }
+
+        private void NextImage()
+        {
+            actualImage++;
+            DisplayImage();
+        }
+
+        private void PreviousImage()
+        {
+            actualImage--;
+            DisplayImage();
+        }
+
+        private void DisplaySmash()
+        {
+            Smash.Content = "Smash: " + smash;
+        }
+
+        private void DisplayPass()
+        {
+            Pass.Content = "Pass: " + pass;
+        }
+
+        private void DisplayImage()
+        {
+            if (actualImage < 0 || actualImage >= numberOfImages)
+            {
+                TestedImage.Source = null;
+                return;
+            }
+
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(pathPrefix + actualImage + pathSuffix);
+            myBitmapImage.DecodePixelWidth = 1800;
+            myBitmapImage.EndInit();
+            TestedImage.Source = myBitmapImage;
+        }
+    }
+}
